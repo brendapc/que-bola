14cf8dc191bd1d0df1b6d8d3bddd8584ca343110
Brenda Pereira <brendacamara@outlook.com>
2021-04-04 11:31:23 -0300

    feat: use api to get data

--- que-bola/src/championships/Championship.jsx
+++ que-bola/src/championships/Championship.jsx
@@ -1,16 +1,32 @@
-import React from "react";
-import { ContentSection, ChampionshipTitle} from './championships-styles'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { ContentSection, ChampionshipTitle } from "./championships-styles";
+
 const Championship = () => {
+  const [data, setData] = useState([]);
+  const [title, setTitle] = useState("");
+  useEffect(() => {
+    async function getData() {
+      const response = await axios.get(
+        "https://api.api-futebol.com.br/v1/campeonatos/10/fases/104",
+        {
+          headers: {
+            Authorization: `Bearer live_e7a21e202a77cd69e356d3dd14a52a`,
+          },
+        }
+      );
+      console.log(response.data);
+      console.log(response.data.campeonato.rodada_atual);
+      setData(response.data.campeonato.rodada_atual.nome);
+      setTitle(response.data.campeonato.nome);
+    }
+    getData();
+  }, []);
   return (
     <div>
       <ContentSection>
-        <ChampionshipTitle>Libertadores</ChampionshipTitle>
-        <div>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut non
-          nesciunt dolorum porro facere vitae? Minima, nam laudantium? Enim
-          maxime repudiandae odio qui porro iste magnam obcaecati ut eos
-          architecto.
-        </div>
+        <ChampionshipTitle>{title}</ChampionshipTitle>
+        <div>{data}</div>
       </ContentSection>
     </div>
   );
