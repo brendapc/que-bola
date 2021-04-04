57add84ea4237096337b7a34a53796568665eed9
Brenda Pereira <brendacamara@outlook.com>
2021-04-03 21:30:33 -0300

    feat: championship sections

---
+++ que-bola/src/championships/Championship.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { ContentSection, ChampionshipTitle} from './championships-styles'
+const Championship = () => {
+  return (
+    <div>
+      <ContentSection>
+        <ChampionshipTitle>Libertadores</ChampionshipTitle>
+        <div>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut non
+          nesciunt dolorum porro facere vitae? Minima, nam laudantium? Enim
+          maxime repudiandae odio qui porro iste magnam obcaecati ut eos
+          architecto.
+        </div>
+      </ContentSection>
+    </div>
+  );
+};
+
+export default Championship;
---
+++ que-bola/src/championships/championships-styles.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const ChampionshipTitle = styled.h1`
+  display: inline-block;
+`;
+
+export const ContentSection = styled.section`
+  background: #b5b6b9;
+  border: 2px solid black;
+  border-radius: 10px;
+  height: 20rem;
+  margin: 2rem;
+`;
