a603c399f5a1240cdd1632b817952c6c8364712b
Brenda Pereira <brendacamara@outlook.com>
2021-04-04 14:40:19 -0300

    feat: new api for matches data

--- que-bola/src/championships/Championship.jsx
+++ que-bola/src/championships/Championship.jsx
@@ -3,30 +3,38 @@ import React, { useEffect, useState } from "react";
 import { ContentSection, ChampionshipTitle } from "./championships-styles";
 
 const Championship = () => {
-  const [data, setData] = useState([]);
   const [title, setTitle] = useState("");
-  useEffect(() => {
-    async function getData() {
-      const response = await axios.get(
-        "https://api.api-futebol.com.br/v1/campeonatos/10/fases/104",
-        {
-          headers: {
-            Authorization: `Bearer live_e7a21e202a77cd69e356d3dd14a52a`,
-          },
-        }
-      );
-      console.log(response.data);
-      console.log(response.data.campeonato.rodada_atual);
-      setData(response.data.campeonato.rodada_atual.nome);
-      setTitle(response.data.campeonato.nome);
-    }
-    getData();
-  }, []);
+  async function getData() {
+    const response = await axios.get(
+      "https://api.football-data.org/v2/competitions/PL/",
+      {
+        headers: {
+          "X-Auth-Token": `a3929fb8a1504c7cae89035aa6535b62`,
+        },
+      }
+    );
+    setTitle(response.data.name);
+    const currentMatchday = response.data.currentSeason.currentMatchday;
+
+    const matchdayData = await axios.get(
+      `http://api.football-data.org/v2/competitions/PL/matches/?matchday=${currentMatchday}`,
+      {
+        headers: {
+          "X-Auth-Token": `a3929fb8a1504c7cae89035aa6535b62`,
+        },
+      }
+    );
+    const matches = matchdayData.data.matches;
+    console.log(matchdayData.data.matches);
+    return matches;
+  }
+  const matchesArray = getData();
+
   return (
     <div>
       <ContentSection>
         <ChampionshipTitle>{title}</ChampionshipTitle>
-        <div>{data}</div>
+        <div></div>
       </ContentSection>
     </div>
   );
