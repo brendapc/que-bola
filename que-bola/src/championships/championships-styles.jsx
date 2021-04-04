33269d039186494914d4c823133ca7939b0c98dd
Brenda Pereira <brendacamara@outlook.com>
2021-04-03 21:57:34 -0300

    refactor: update style of championships sections

--- que-bola/src/championships/championships-styles.jsx
+++ que-bola/src/championships/championships-styles.jsx
@@ -2,12 +2,14 @@ import styled from "styled-components";
 
 export const ChampionshipTitle = styled.h1`
   display: inline-block;
+  border-bottom: 5px solid #9ede73;
 `;
 
 export const ContentSection = styled.section`
-  background: #e5e4e2;
-  border: 2px solid black;
+  background: #e6e4e4;
+  box-shadow: 0px 0px 24px 5px rgb(30 30 30 / 51%);
   border-radius: 10px;
   height: 20rem;
   margin: 2rem;
+  padding: 0 1rem;
 `;
