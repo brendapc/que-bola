b79db0046ee1fd558ca374cb1bb2c3ea7cc55e64
Brenda Pereira <brendacamara@outlook.com>
2021-04-05 20:37:33 -0300

    feat: getting data by championship

--- que-bola/src/championships/Championship.jsx
+++ que-bola/src/championships/Championship.jsx
@@ -9,13 +9,13 @@ import {
 } from "./championships-styles";
 import { Card } from "../layout-structure/styled-components/Card";
 
-const Championship = () => {
+const Championship = ({ championship }) => {
   const [title, setTitle] = useState("");
   const [data, setData] = useState([]);
 
   const getData = async () => {
-    /*  const response = await axios.get(
-      "https://api.football-data.org/v2/competitions/PL/",
+    const response = await axios.get(
+      `https://api.football-data.org/v2/competitions/${championship}/`,
       {
         headers: {
           "X-Auth-Token": `a3929fb8a1504c7cae89035aa6535b62`,
@@ -24,15 +24,23 @@ const Championship = () => {
     );
     setTitle(response.data.name);
     const currentMatchday = response.data.currentSeason.currentMatchday;
- */
+
     const matchdayData = await axios.get(
-      `http://api.football-data.org/v2/competitions/PL/matches/?matchday=${30}`,
+      `http://api.football-data.org/v2/competitions/${championship}/matches/?matchday=${currentMatchday}`,
       {
         headers: {
           "X-Auth-Token": `a3929fb8a1504c7cae89035aa6535b62`,
         },
       }
     );
+   /*  const withSeason = await axios.get(
+      `http://api.football-data.org/v2/competitions/${championship}/matches/?matchday=${currentMatchday}&season=${2020}`,
+      {
+        headers: {
+          "X-Auth-Token": `a3929fb8a1504c7cae89035aa6535b62`,
+        },
+      }
+    ); */
     const matches = matchdayData.data.matches;
     setData(matches);
     return matches;
@@ -45,7 +53,7 @@ const Championship = () => {
   return (
     <div>
       <ContentSection>
-        <ChampionshipTitle>Premiere League</ChampionshipTitle>
+        <ChampionshipTitle>{title}</ChampionshipTitle>
         <Content>
           <CardsWraper>
             {data.length > 1 && data.map((match) => <Card matchInfo={match} />)}
