41157f1c069547712bebf57a003896b8abf5a41b
Brenda Pereira <brendacamara@outlook.com>
2021-04-05 20:36:17 -0300

    fix: layout

--- que-bola/src/championships/championships-styles.jsx
+++ que-bola/src/championships/championships-styles.jsx
@@ -9,18 +9,19 @@ export const ContentSection = styled.section`
   background: #e6e4e4;
   box-shadow: 0px 0px 24px 5px rgb(30 30 30 / 51%);
   border-radius: 10px;
-  height: 20rem;
+  height: 25rem;
   margin: 2rem;
   padding: 0 1rem;
 `;
 
 export const Content = styled.div`
-  height: 14rem;
+  height: 20rem;
   overflow-y: auto;
   display: flex;
 `;
 
 export const ContentCard = styled.div`
+  height: 16rem;
   width: 20rem;
   margin: 0.5rem;
   background: #fff;
@@ -29,6 +30,7 @@ export const ContentCard = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-around;
+  padding: 1rem;
 `;
 
 export const CardsWraper = styled.div`
