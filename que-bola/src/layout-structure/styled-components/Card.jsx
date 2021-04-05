e9c305e17d38de282e1b4810d43ea4ea67f98fde
Brenda Pereira <brendacamara@outlook.com>
2021-04-05 20:36:42 -0300

    feat: add team logos

--- que-bola/src/layout-structure/styled-components/Card.jsx
+++ que-bola/src/layout-structure/styled-components/Card.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { ContentCard } from "../../championships/championships-styles";
 
@@ -11,6 +11,7 @@ const Standing = styled.div`
 const Versus = styled.div`
   font-size: 3rem;
   color: #5ac8e9;
+  align-self: center;
 `;
 const Goals = styled.h3`
   font-size: 2rem;
@@ -18,27 +19,54 @@ const Goals = styled.h3`
 `;
 const Team = styled.h2`
   font-size: 1rem;
+  align-self: center;
 `;
 const MatchScore = styled.div`
   display: grid;
-  grid-template-rows: 1fr 4rem;
+  grid-template-rows: 1fr auto 4rem;
 `;
+
+const Img = styled.img`
+  width: 100px;
+  height: 100px;
+  align-self: center;
+  justify-self: center;
+`;
+
+const Hour = styled.p`
+  margin: 0;
+`;
+
 export const Card = ({ matchInfo }) => {
+  const [time, setTime] = useState("");
+  useEffect(() => {
+    const dateUTC = new Date(matchInfo.utcDate);
+    const dateLocaled = new Date(dateUTC.toString());
+    setTime(
+      `${dateLocaled.getHours()}:${
+        dateLocaled.getMinutes().length > 1 ? dateLocaled.getMinutes() : "00"
+      }`
+    );
+  }, []);
+  const awayLogo = `https://crests.football-data.org/${matchInfo.awayTeam.id}.svg`;
+  const homeLogo = `https://crests.football-data.org/${matchInfo.homeTeam.id}.svg`;
   if (matchInfo)
     return (
       <ContentCard>
         <Standing>
           <MatchScore>
             <Team>{matchInfo.homeTeam.name}</Team>
+            <Img src={awayLogo} alt="" />
             <Goals>{matchInfo.score.fullTime.homeTeam}</Goals>
           </MatchScore>
           <Versus> X </Versus>
           <MatchScore>
             <Team>{matchInfo.awayTeam.name}</Team>
+            <Img src={homeLogo} alt="" />
             <Goals>{matchInfo.score.fullTime.awayTeam}</Goals>
           </MatchScore>
         </Standing>
-        <p>Horário</p>
+        <Hour>{time}</Hour>
       </ContentCard>
     );
   else {
